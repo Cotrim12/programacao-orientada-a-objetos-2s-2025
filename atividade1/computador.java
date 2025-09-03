42223958d7e645e4f2c054e10cde961c152a0a3a
Cotrim12 <cotrimdasilvafelippe908@gmail.com>
2025-09-03 12:39:41 -0300

    terminar em casa

---
+++ atividade1/computador.java
@@ -0,0 +1,17 @@
+package atividade1;
+
+public class computador {
+
+    private Integer memoriaram;
+    private Integer ssd;
+    private Integer nucleos;
+    private float ops;
+    
+    private computador(Integer memoriaram, Integer ssd, Integer nucleos, float ops){
+        this.memoriaram = memoriaram;
+        this.ssd = ssd;
+        this.nucleos = nucleos;
+        this.ops = ops;
+
+    }
+}
---
+++ atividade1/programa.java
@@ -0,0 +1,17 @@
+package atividade1;
+
+public class programa {
+    public Integer memoriaRamAlocada;
+    public Integer ssdocupado;
+    public Integer nucleos;
+    public Integer qO;
+
+
+public programa(Integer memoriaRamAlocada, Integer ssdocupado, Integer nucleos, Integer qO){
+    this.memoriaRamAlocada = memoriaRamAlocada;
+    this.ssdocupado = ssdocupado;
+    this.nucleos = nucleos;
+    this.qO = qO;
+
+}
+}
